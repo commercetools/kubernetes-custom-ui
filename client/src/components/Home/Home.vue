099a9fea5a04f02a5f377ec7e6a134b3847d796d
Javier Ortiz <javier.ortizsaorin@commercetools.de>
2018-05-24 15:01:21 +0200

    Add client template based on Vue.js
    It's based on the vue-cli webpack template (https://github.com/vuejs-templates/webpack) and includes among other things:
    
    * Jest
    * Babel
    * Webpack
    * ESLint

---
+++ client/src/components/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home"><h1>{{ message }}</h1></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+    };
+  },
+};
+</script>
+<style lang="scss">
+.home {
+  text-align: center;
+}
+</style>
+
