5df640bfd097ed10d2f98a481469ff87fbf7c087
Javier Ortiz <javier.ortizsaorin@commercetools.de>
2018-06-02 15:36:09 +0200

    Add navigation guards in the router

--- client/src/main.js
+++ client/src/main.js
@@ -7,14 +7,22 @@ import VueProgressBar from 'vue-progressbar'
 import App from './App'
 import Store from './store'
 import Router from './router'
+import UtilsAuthentication from './utils/authentication/utils.authentication'
+import Authentication from './plugins/authentication'
 
 Vue.config.productionTip = false
 
 const store = Store()
-const router = Router()
+const utilsAuthentication = UtilsAuthentication()
+const authentication = Authentication({
+  authStore: store.state.authentication,
+  utilsAuthentication,
+})
+const router = Router({ authentication })
 
 Vue.use(BootstrapVue)
 Vue.use(VueProgressBar, { color: '#3371e3', failedColor: 'red', thickness: '2px' })
+Vue.use(authentication)
 
 axios.defaults.baseURL = process.env.API_URL || '/'
 
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,10 +3,29 @@ import Router from 'vue-router'
 import SignIn from '@/components/SignIn'
 import Cronjobs from '@/components/Cronjobs'
 
-export default function () {
+export default ({ authentication }) => {
   Vue.use(Router)
 
   const scrollBehavior = (to, from, savedPosition) => savedPosition || { x: 0, y: 0 }
+
+  const checkAuthentication = (to, from, next) => {
+    if (
+      to.matched.some(record => record.meta.requiresAuthentication) &&
+      !authentication.isUserAuthenticated()
+    )
+      // if not authenticated we redirect to the Home page
+      return next({ name: 'Home' })
+
+    return next()
+  }
+
+  const ifToHomeAndAuthenticated = (to, from, next) => {
+    if (to.name === 'Home' && authentication.isUserAuthenticated())
+      // if Home and authenticated we redirect to the Cronjobs page
+      return next({ name: 'Cronjobs' })
+
+    return next()
+  }
 
   const router = new Router({
     mode: 'history',
@@ -14,16 +33,20 @@ export default function () {
     routes: [
       {
         path: '/',
-        name: 'SignIn',
+        name: 'Home',
         component: SignIn,
       },
       {
         path: '/cronjobs',
         name: 'Cronjobs',
         component: Cronjobs,
+        meta: { requiresAuthentication: true },
       },
     ],
   })
+
+  router.beforeResolve(checkAuthentication)
+  router.beforeResolve(ifToHomeAndAuthenticated)
 
   return router
 }
